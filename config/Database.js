9b2000ad52f5cc51f97d939b3162e2c05f509597
zaq <85076901+zaq99@users.noreply.github.com>
2023-02-01 22:13:36 +0700

    set env

--- config/Database.js
+++ config/Database.js
@@ -1,20 +1,38 @@
-import { Sequelize } from "sequelize";
+// import { Sequelize } from "sequelize";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const { solusi_db } = process.env;
+const { DB_URI } = process.env;
 
 // const db = new Sequelize("solusi_db", "root", "", {
 //     host: "localhost",
 //     dialect: "mysql",
 // });
 
-const db = new Sequelize(solusi_db, {
-    define: {
-        timestamps: false,
-    },
-});
+// // ---------------- mongodb
+// const db = new Sequelize(solusi_db, {
+//     define: {
+//         timestamps: false,
+//     },
+// });
+// // ------------------- mongodb
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log("MongoDB Connected...");
+    } catch (err) {
+        console.error(err.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+};
 
 export default db;
 // export lalu buat database pada xampp
